c3f5d3427a3c5e930bdbd445349246800f1d52e6
francisca <franciscawanjiru746@gmail.com>
2025-09-16 16:46:34 +0300

    fixed librarian function

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -25,16 +25,3 @@ def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
     return library.librarian
 
-
-if __name__ == "__main__":
-    # Example usage
-    print("Books by Author John Doe:")
-    for book in get_books_by_author("John Doe"):
-        print(book.title)
-
-    print("\nBooks in Central Library:")
-    for book in get_books_in_library("Central Library"):
-        print(book.title)
-
-    print("\nLibrarian of Central Library:")
-    print(get_librarian_for_library("Central Library"))
