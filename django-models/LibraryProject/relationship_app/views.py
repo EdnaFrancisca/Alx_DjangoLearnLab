5bf64ce9ac92fc06f2a2a249b47853191905dfd4
francisca <franciscawanjiru746@gmail.com>
2025-09-16 20:43:06 +0300

    html changes

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
 from django.views.generic.detail import DetailView
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
