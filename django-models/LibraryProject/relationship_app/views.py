7adbc293b1fb39918ecce43a44d5163d656edc58
francisca <franciscawanjiru746@gmail.com>
2025-09-16 20:54:33 +0300

    logins

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,25 @@
-from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth import login
 from django.views.generic.detail import DetailView
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404 ,redirect
 from django.views.generic import DetailView
 from .models import Library,Book
+
+
+# Registration view
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # automatically log in new user
+            return redirect("home")  # make sure you have a "home" URL configured
+    else:
+        form = UserCreationForm()
+
+    # ✅ explicitly referencing relationship_app/register.html
+    return render(request, "relationship_app/register.html", {"form": form})
 
 
 def list_books(request):
