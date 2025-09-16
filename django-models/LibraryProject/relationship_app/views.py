f77f676af82ab65995041c0ecf21b8f2628f018d
francisca <franciscawanjiru746@gmail.com>
2025-09-16 20:01:04 +0300

    bug fixing

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,11 +4,9 @@ from django.views.generic import DetailView
 from .models import Book, Library
 
 
-# Function-based view: list all books with their authors
 def book_list(request):
-    books = Book.objects.select_related("author").all()
-    output = "\n".join([f"{book.title} by {book.author.name}" for book in books])
-    return HttpResponse(output, content_type="text/plain")
+    books = Book.objects.all()  #
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
 
 class LibraryDetailView(DetailView):
